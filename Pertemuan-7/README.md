c591777a60738692081f3b267fc8004a76895471
Diah Putri Nofianti <diahputrinofianti@gmail.com>
2023-10-12 09:43:16 +0700

    update project

--- Pertemuan-7/README.md
+++ Pertemuan-7/README.md
@@ -7,4 +7,4 @@
 
 ## Praktikum 2: Langkah Awal
 Teks yang ditampilkan dalam aplikasi berupa nama lengkap, lalu simpan file screenshot dengan nama `01.png` pada folder images (buat folder baru jika belum ada) di project `flutter_basic` ini.
-![Screenshot 1](images/01.png)
+![Screenshot 1](images/01.jpeg)
--- Pertemuan-7/layout_flutter/lib/main.dart
+++ Pertemuan-7/layout_flutter/lib/main.dart
@@ -5,42 +5,43 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32)
-  child: Row(
-    children: [
-      Expanded(
-        /* soal 1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /* soal 2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Wisata Gunung di Batu',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+  Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+          /* soal 1*/
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /* soal 2*/
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  'Wisata Gunung di Batu',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-            ),
-            Text(
-              'Batu, Malang, Indonesia',
-              style: TextStyle(
-                color: Colors.grey[500],),
-            ),
-          ],
+              Text(
+                'Batu, Malang, Indonesia',
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      /* soal 3*/
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      const Text('41'),
-    ],
-  ),
-);
+        /* soal 3*/
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        const Text('41'),
+      ],
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
